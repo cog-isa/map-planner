5409986aebb852f06d95e02e80d8ccc6a274b86d
KiselevGA <glebkiselev777@mail.ru>
2018-02-27 17:27:43 +0300

    full filling signs world with spatial signs

--- src/grounding/spatial_cognition/map_signs.py
+++ src/grounding/spatial_cognition/map_signs.py
@@ -1,7 +1,7 @@
 import logging
 import cv2
 import numpy as np
-import re
+import itertools
 
 from grounding.semnet import Sign
 
@@ -127,6 +127,7 @@ def size_founder(reg_loc, obj_loc, ag):
     else:
         size = dislocations[target]
     #TODO while objects and agents have radius >= cell/2
+    # TODO 2 obj too clear 1 to another
     return size
 
 
@@ -139,7 +140,7 @@ def map_recogn(map_path, types, agent):
     """
     obj_loc = {}
     block_size = 20
-    elements = 8
+    elements = 5
     # map to ndarray
     image = cv2.imread(map_path)
 
@@ -156,6 +157,10 @@ def map_recogn(map_path, types, agent):
             centre = round(place[num][0][0]+(block_size*0.5)), round(place[num][0][1]+(block_size*0.5))
             obj_loc.setdefault(type, []).append((type+str(len(obj_loc[type])), centre))
 
+    # add agent
+    ag_centre = [i for i in place if not i in subList][0]
+    centre = round(ag_centre[0][0] + (block_size * 0.5)), round(ag_centre[0][1] + (block_size * 0.5))
+    obj_loc.setdefault(agent, []).append((agent, centre))
     # division into regions
     region_location, region_map = locater(image, 'region', obj_loc)
 
@@ -166,7 +171,7 @@ def map_recogn(map_path, types, agent):
         if agent in value:
             size = size_founder(region_location[key], obj_loc, agent)
             break
-
+    # TODO cell_map test
     cell_location, cell_map = cell_creater(size, obj_loc, region_location)
 
     return region_map, cell_map, cell_location
@@ -189,17 +194,24 @@ def ground(map_file, object_types, agent):
     signs[I_sign.name] = I_sign
     obj_signifs[They_sign] = They_sign.add_significance()
     signs[They_sign.name] = They_sign
-
     # create sign map and matrix
     Map_sign = Sign("Map")
     obj_signifs[Map_sign] = Map_sign.add_significance()
     signs[Map_sign.name] = Map_sign
-
-
     # create regions signs and matrixes
     Region_sign = Sign("Region")
-    obj_signifs[Region_sign] = Region_sign.add_significance()
+    region_signif = Region_sign.add_significance()
+    obj_signifs[Region_sign] = region_signif
     signs[Region_sign.name] = Region_sign
+    regions = []
+    for role in ('?X', '?Y', '?Z'):
+        Region_signs = Sign('Region'+role)
+        regions.append(Region_signs)
+        role_signif = Region_signs.add_significance()
+        obj_signifs[Region_signs] = role_signif
+        signs[Region_signs.name] = Region_signs
+        connector = role_signif.add_feature(region_signif, zero_out=True)
+        Region_sign.add_out_significance(connector)
 
     for region in region_map:
         Regions_sign = Sign(region)
@@ -208,6 +220,7 @@ def ground(map_file, object_types, agent):
         Region_signif = Region_sign.add_significance()
         connector = Region_signif.add_feature(obj_signifs[Regions_sign], zero_out=True)
         Regions_sign.add_out_significance(connector)
+
 
     # create cell signs and matrixes
     Cell_sign = Sign("Cell")
@@ -222,6 +235,10 @@ def ground(map_file, object_types, agent):
     obj_signifs[Celly_sign] = Celly_sign.add_significance()
     signs[Celly_sign.name] = Celly_sign
 
+    Celly_signif = Celly_sign.add_significance()
+    con = Celly_signif.add_feature(obj_signifs[Cell_sign], zero_out=True)
+    Cell_sign.add_out_significance(con)
+
     for cell, value in cell_map.items():
         Cells_sign = Sign(cell)
         obj_signifs[Cells_sign] = Cells_sign.add_significance()
@@ -233,10 +250,8 @@ def ground(map_file, object_types, agent):
             Cellx_signif = Cellx_sign.add_significance()
             con = Cellx_signif.add_feature(obj_signifs[Cells_sign], zero_out=True)
             Cells_sign.add_out_significance(con)
-        else:
-            Celly_signif = Celly_sign.add_significance()
-            con = Celly_signif.add_feature(Cell_signif, zero_out=True)
-            Cell_sign.add_out_significance(con)
+
+
 
     # create objects signs
     Object_sign = Sign('Object')
@@ -262,12 +277,210 @@ def ground(map_file, object_types, agent):
         connector = Object_sign_signif.add_feature(obj_signifs[s], zero_out=True)
         s.add_out_significance(connector)
 
-    print()
-    # locate objects
-    pass
+    dirs = []
+    Direction_sign = Sign('Direction')
+    dirs.append(Direction_sign)
+    obj_signifs[Direction_sign] = Direction_sign.add_significance()
+    signs[Direction_sign.name] = Direction_sign
+
+    Direction_start = Sign('Dir-start')
+    dirs.append(Direction_start)
+    Dir_signif = Direction_start.add_significance()
+    obj_signifs[Direction_start] = Dir_signif
+    signs[Direction_start.name] = Direction_start
+    connector = Dir_signif.add_feature(obj_signifs[Direction_sign], zero_out=True)
+    Direction_sign.add_out_significance(connector)
+
+    Direction_finish = Sign('Dir-finish')
+    dirs.append(Direction_finish)
+    Dir_signif = Direction_finish.add_significance()
+    obj_signifs[Direction_finish] = Dir_signif
+    signs[Direction_finish.name] = Direction_finish
+    connector = Dir_signif.add_feature(obj_signifs[Direction_sign], zero_out=True)
+    Direction_sign.add_out_significance(connector)
+
+    directions = ['Above', 'Below', 'Left', 'Right', 'Above-left', 'Above-right', 'Below-left', 'Below-right']
+
+    for dir in directions:
+        dir_sign = Sign(dir)
+        dir_signif = dir_sign.add_significance()
+        obj_signifs[dir_sign] = dir_signif
+        signs[dir_sign.name] = dir_sign
+        Direction_signif = Direction_sign.add_significance()
+        connector = Direction_signif.add_feature(dir_signif, zero_out=True)
+        dir_sign.add_out_significance(connector)
+
+    distance = []
+    # locate objects: include
+    Include_sign = Sign('Include')
+    distance.append(Include_sign)
+    signs[Include_sign.name] = Include_sign
+    obj_signifs[Include_sign] = Include_sign.add_significance()
+    for obj in obj_signs:
+        Include_signif = Include_sign.add_significance()
+        connector = Include_signif.add_feature(obj_signifs[Region_sign], effect=False, zero_out=True)
+        Region_sign.add_out_significance(connector)
+        connector = Include_signif.add_feature(obj_signifs[obj], effect=True, zero_out=True)
+        obj.add_out_significance(connector)
+    #TODO test random cell generation place
+    for region, cells in cell_location.items():
+        region_sign = signs[region]
+        for cell in cells:
+            Include_signif = Include_sign.add_significance()
+            connector = Include_signif.add_feature(obj_signifs[region_sign], effect=False, zero_out=True)
+            region_sign.add_out_significance(connector)
+            cell_sign = signs[cell]
+            connector = Include_signif.add_feature(obj_signifs[cell_sign], effect=True, zero_out=True)
+            cell_sign.add_out_significance(connector)
+    #include cellX cellY
+    Include_X_signif = Include_sign.add_significance()
+    connector = Include_X_signif.add_feature(obj_signifs[Region_sign], effect=False, zero_out=True)
+    Region_sign.add_out_significance(connector)
+    connector = Include_X_signif.add_feature(obj_signifs[Cellx_sign], effect=True, zero_out=True)
+    Cellx_sign.add_out_significance(connector)
+    Include_Y_signif = Include_sign.add_significance()
+    connector = Include_Y_signif.add_feature(obj_signifs[Region_sign], effect=False, zero_out=True)
+    Region_sign.add_out_significance(connector)
+    connector = Include_Y_signif.add_feature(obj_signifs[Celly_sign], effect=True, zero_out=True)
+    Celly_sign.add_out_significance(connector)
+    # locate objects: contain
+    ag_cell = []
+    Contain_sign = Sign('Contain')
+    contain_signif = Contain_sign.add_significance()
+    obj_signifs[Contain_sign] = contain_signif
+    for cell, objs in cell_map.items():
+        cell_sign = signs[cell]
+        if len(objs) > 1 and 0 in objs:
+            for obj in objs:
+                if obj !=0:
+                    contain_signif = Contain_sign.add_significance()
+                    connector = contain_signif.add_feature(obj_signifs[cell_sign], effect=False, zero_out=True)
+                    cell_sign.add_out_significance(connector)
+                    if obj in signs:
+                        obj_sign = signs[obj]
+                    else:
+                        obj_sign = Sign(obj)
+                        if 'agent' in obj:
+                            ag_cell.append(cell)
+                        signs[obj_sign.name] = obj_sign
+                        obj_signifs[obj_sign] = obj_sign.add_significance()
+                        obj_role = obj.title()[:-1]
+                        role_names = [s.name for s in obj_signs]
+                        if obj_role in role_names:
+                            obj_role_sign = signs[obj_role]
+                            orss = obj_role_sign.add_significance()
+                            con = orss.add_feature(obj_signifs[obj_sign], zero_out=True)
+                            obj_sign.add_out_significance(con)
+                    connector = contain_signif.add_feature(obj_signifs[obj_sign], effect=True, zero_out=True)
+                    obj_sign.add_out_significance(connector)
+
+        else:
+            contain_signif = Contain_sign.add_significance()
+            connector = contain_signif.add_feature(obj_signifs[cell_sign], effect=False, zero_out=True)
+            cell_sign.add_out_significance(connector)
+            connector = contain_signif.add_feature(obj_signifs[Nothing_sign], effect=True, zero_out=True)
+            Nothing_sign.add_out_significance(connector)
+
+    # closely
+    Closely_sign = Sign('Closely')
+    distance.append(Closely_sign)
+    signs[Closely_sign.name] = Closely_sign
+    Closely_signif = Closely_sign.add_significance()
+    obj_signifs[Closely_sign] = Closely_signif
+    # locate cells: closely
+    Closely_cells_signif = Closely_sign.add_significance()
+    connector = Closely_cells_signif.add_feature(obj_signifs[Cellx_sign])
+    Cellx_sign.add_out_significance(connector)
+    connector = Closely_cells_signif.add_feature(obj_signifs[Celly_sign])
+    Celly_sign.add_out_significance(connector)
+    # locate regions: closely
+    combinations = list(itertools.combinations(regions, 2))
+    closely_regions = []
+    for combination in combinations:
+        Closely_signif = Closely_sign.add_significance()
+        closely_regions.append(Closely_signif)
+        connector = Closely_signif.add_feature(obj_signifs[combination[0]])
+        combination[0].add_out_significance(connector)
+        connector = Closely_signif.add_feature(obj_signifs[combination[1]])
+        combination[1].add_out_significance(connector)
+    # locate cell and regions: closely
+    closely_regcells = []
+    for reg in regions:
+        Closely_signif = Closely_sign.add_significance()
+        closely_regcells.append(Closely_signif)
+        connector = Closely_signif.add_feature(obj_signifs[Cellx_sign])
+        Cellx_sign.add_out_significance(connector)
+        connector = Closely_signif.add_feature(obj_signifs[reg])
+        reg.add_out_significance(connector)
+    for reg in regions:
+        Closely_signif = Closely_sign.add_significance()
+        closely_regcells.append(Closely_signif)
+        connector = Closely_signif.add_feature(obj_signifs[Celly_sign])
+        Celly_sign.add_out_significance(connector)
+        connector = Closely_signif.add_feature(obj_signifs[reg])
+        reg.add_out_significance(connector)
+    # nearly
+    Nearly_sign = Sign('Nearly')
+    distance.append(Nearly_sign)
+    signs[Nearly_sign.name] = Nearly_sign
+    Nearly_signif = Nearly_sign.add_significance()
+    obj_signifs[Nearly_sign] = Nearly_signif
+    # locate cell and regions: nearly
+    cell_y_reg = [cm for cm in closely_regcells if Celly_sign in cm.get_signs()]
+    for cm in cell_y_reg:
+        Nearly_signif = Nearly_sign.add_significance()
+        con = Nearly_signif.add_feature(Closely_cells_signif)
+        Closely_sign.add_out_significance(con)
+        conn = Nearly_signif.add_feature(cm)
+        Closely_sign.add_out_significance(conn)
+
+    # locate regions: nearly
+    Nearly_signif_reg = Nearly_sign.add_significance()
+    Region_X_sign = signs['Region?X']
+    Region_Y_sign = signs['Region?Y']
+    Region_Z_sign = signs['Region?Z']
+    closely_XZ = [cm for cm in closely_regions if Region_X_sign in cm.get_signs() and Region_Z_sign in cm.get_signs()][0]
+    closely_ZY = [cm for cm in closely_regions if Region_Z_sign in cm.get_signs() and Region_Y_sign in cm.get_signs()][0]
+    con = Nearly_signif_reg.add_feature(closely_XZ)
+    Closely_sign.add_out_significance(con)
+    conn = Nearly_signif_reg.add_feature(closely_ZY)
+    Closely_sign.add_out_significance(conn)
+
+    # faraway
+    Faraway_sign = Sign('Faraway')
+    distance.append(Faraway_sign)
+    signs[Faraway_sign.name] = Faraway_sign
+    obj_signifs[Faraway_sign] = Faraway_sign.add_significance()
+    Faraway_signif = Faraway_sign.add_significance()
+    con = Faraway_signif.add_feature(Nearly_signif_reg)
+    Nearly_sign.add_out_significance(con)
+    conn = Faraway_signif.add_feature(Include_X_signif)
+    Nearly_sign.add_out_significance(conn)
+
+    # distance
+    Distance_sign = Sign('Distance')
+    signs[Distance_sign.name] = Distance_sign
+    Distance_signif = Distance_sign.add_significance()
+    obj_signifs[Distance_sign] = Distance_signif
+    for dist in distance:
+        Distance_signif = Distance_sign.add_significance()
+        connector = Distance_signif.add_feature(obj_signifs[dist])
+        dist.add_out_significance(connector)
+    # location
+    Location_sign = Sign('Location')
+    signs[Location_sign.name] = Location_sign
+    obj_signifs[Location_sign] = Location_sign.add_significance()
+    for dir in dirs:
+        Location_signif = Location_sign.add_significance()
+        conn = Location_signif.add_feature(obj_signifs[Distance_sign], effect=False)
+        Distance_sign.add_out_significance(conn)
+        con = Location_signif.add_feature(obj_signifs[dir], effect=True)
+        dir.add_out_significance(con)
+
+
 
 if __name__ == "__main__":
-    object_types = ["agent", "table", "block"]
+    object_types = ["table", "block"]
     map_file = 'test.jpg'
     agent = 'agent0'
     ground(map_file, object_types, agent)
